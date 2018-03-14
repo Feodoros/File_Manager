46196191a5da0ec02b27d1cd4e8a161a6ea656f1
Feodoros <fzhilkin@mail.ru>
2018-03-15 02:51:22 +0300

    Добавьте файлы проекта.

---
+++ FileManager/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button100 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button100
+            // 
+            this.button100.BackColor = System.Drawing.Color.MistyRose;
+            this.button100.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button100.Location = new System.Drawing.Point(54, 94);
+            this.button100.Name = "button100";
+            this.button100.Size = new System.Drawing.Size(145, 69);
+            this.button100.TabIndex = 0;
+            this.button100.Text = "Навигация по папкам и файлам.";
+            this.button100.UseVisualStyleBackColor = false;
+            this.button100.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MistyRose;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(267, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Копирование и перемещение файлов и папок.";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MistyRose;
+            this.button2.Location = new System.Drawing.Point(267, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 69);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Архивация файлов и папок.";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MistyRose;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(54, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 69);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Переименование, удаление.";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.AliceBlue;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(33, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 28);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Выберите действие:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(465, 314);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button100);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button100;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ FileManager/Form2.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileManager
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public void Output_Folders() //Вывод папок на панель по заданному адресу.
+        {
+            listBox1.Items.Clear();
+
+            DirectoryInfo directory = new DirectoryInfo(textBox1.Text);
+
+            DirectoryInfo[] directoryS = directory.GetDirectories();
+
+            foreach (DirectoryInfo directoryX in directoryS)
+            {
+                listBox1.Items.Add(directoryX);
+            }
+
+            FileInfo[] Files = directory.GetFiles();
+
+            foreach (FileInfo FileX in Files)
+            {
+                listBox1.Items.Add(FileX);
+            }
+        }
+
+
+        //Действия кнопок и элементов формы.   
+        private void button1_Click(object sender, EventArgs e) //Кнопка "Перейти".
+        {
+            Output_Folders();
+        }
+
+        private void button2_Click(object sender, EventArgs e)  //Кнопка "Назад".
+        {
+            string s = textBox1.Text;
+            if (s[s.Length - 1] == '\\')
+            {
+                s = s.Remove(s.Length - 1, 1);
+                while (s[s.Length - 1] != '\\')
+                {
+                    s = s.Remove(s.Length - 1, 1);
+                }
+            }
+            else
+            {
+                while (s[s.Length - 1] != '\\')
+                {
+                    s = s.Remove(s.Length - 1, 1);
+                }
+            }
+            textBox1.Text = s;
+            Output_Folders();
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e) //Двойной щелчок по элементу в listbox'е.
+        {
+            if (Path.GetExtension(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString())) == "")
+            {
+                textBox1.Text = Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString());
+                Output_Folders();
+            }
+            else
+            {
+                Process.Start(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString()));
+            }
+        }
+
+        /*private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+       {
+           // вызываем главную форму, которая открыла текущую, главная форма всегда = 0 - [0]
+           Form ifrm = Application.OpenForms[0];
+           ifrm.StartPosition = FormStartPosition.Manual; // меняем параметр StartPosition у Form1, иначе она будет использовать тот, который у неё прописан в настройках и всегда будет открываться по центру экрана
+           ifrm.Left = this.Left; // задаём открываемой форме позицию слева равную позиции текущей формы
+           ifrm.Top = this.Top; // задаём открываемой форме позицию сверху равную позиции текущей формы
+           ifrm.Show(); // отображаем Form1
+       }*/
+    }
+}
